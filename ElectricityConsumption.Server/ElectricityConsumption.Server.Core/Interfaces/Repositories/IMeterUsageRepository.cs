97ef6fa4baef4a5b2ee86a32c43b3c588da2fd53
Dishan Rajapaksha <hello@dishanrajapaksha.com>
2022-05-10 17:04:44 +0530

    Update IMeterUsageRepository.cs

--- ElectricityConsumption.Server/ElectricityConsumption.Server.Core/Interfaces/Repositories/IMeterUsageRepository.cs
+++ ElectricityConsumption.Server/ElectricityConsumption.Server.Core/Interfaces/Repositories/IMeterUsageRepository.cs
@@ -1,6 +1,7 @@
 ﻿using ElectricityConsumption.Protos;
 
 namespace ElectricityConsumption.Server.Core.Interfaces.Repositories;
+
 public interface IMeterUsageRepository
 {
     public Task<MeterUsageResponse> Get(MeterUsageReadRequest request);
